48bc0d699032b40d7aef636c0188e0a2d21b3731
WIN-47TDCI7SIDM\SEHC <congdev95@gmail.com>
2023-08-16 15:00:55 +0700

    add trigger

---
+++ trigger.py
@@ -0,0 +1,37 @@
+from google.cloud import bigquery
+import os
+
+def load_data_to_bigquery(event, context):
+    bucket = event['bucket']
+    file_name = event['name']
+
+    project_id = "my-project-25072023-393906"
+    dataset_id = "tiki-dataset"
+    table_id = "tiki-products"
+
+    client = bigquery.Client(project=project_id)
+
+    source_uri = f"gs://{bucket}/{file_name}"
+
+
+    schema_path = "/tmp/schema.json"
+    with open(schema_path, "r") as schema_file:
+        schema = schema_file.read()
+
+    # Tạo job load dữ liệu từ GCS vào BigQuery
+    job_config = bigquery.LoadJobConfig(
+        autodetect=True,
+        source_format=bigquery.SourceFormat.NEWLINE_DELIMITED_JSON,
+        schema=schema,
+    )
+
+    table_ref = client.dataset(dataset_id).table(table_id)
+    job = client.load_table_from_uri(
+        source_uri, table_ref, job_config=job_config
+    )
+
+    job.result()
+    )
+
+    job.result()
+    print(f"Data loaded from {source_uri} to BigQuery table {table_id}")
